32801dbde1183c07a417b47343c089b0ced0f848
Iris Vonberg <irisavonberg@gmail.com>
2023-02-15 14:55:21 -0800

    learning buttons

--- js/github.js
+++
@@ -1,7 +0,0 @@
-const callback = function(result) {
-    $('.js-github-container').append( 
-        result.map( item => `<li>${item.name}</li>` ).join('') 
-    );
-}
-$.get('https://api.github.com/users/facebook/repos')
- .done(callback);
